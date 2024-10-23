8dcd35307935c27b676f1e994244df5b16d23da9
sohangith <shresthasohan013@gmail.com>
2024-10-23 13:31:09 +0545

    multiply and division added to calc

--- calculation.cpp
+++ calculation.cpp
@@ -5,6 +5,10 @@ int main () {
     int a=3;
     int b=4;
     int sum = a+b;
-    cout <<"the sum of two number is " << sum;
+    int multiply=a*b;
+    int div= b/a;
+    cout <<"the sum of two number is" << sum << endl;
+    cout <<"the multilbe of two number is\n " << multiply<<endl;
+    cout <<" the division of two number is " << div;
     return 0;
 }
