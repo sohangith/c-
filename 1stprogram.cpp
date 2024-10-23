d4577d9bdbdccdf076e0da3fc8d68c7c59f566a7
sohangith <shresthasohan013@gmail.com>
2024-10-23 12:22:22 +0545

    first commit

---
+++ 1stprogram.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! whats up\t";
+  cout << "I am learning \"c++\"\n" ;
+  cout << 3+3;
+  return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myage;
+  myage = 15;
+  cout <<"i am " << myage <<" years old";
+  return 0;
+}
